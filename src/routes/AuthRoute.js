f8388c17282da2c94960621810377159a174278b
SilenceBOYZ <92418198+SilenceBOYZ@users.noreply.github.com>
2024-06-14 12:59:43 +0700

    Delete src directory

--- src/routes/AuthRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const AuthenticatorController = require("../app/controller/Authenticator");
-
-router.post("/signin", AuthenticatorController.signIn)
-router.post("/signup", AuthenticatorController.signUp)
-router.get("/get-userInfor", AuthenticatorController.getUser)
-
-module.exports = router;
